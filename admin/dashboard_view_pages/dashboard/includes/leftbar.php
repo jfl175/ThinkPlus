74e98ccb4acc31ac1c405f8d468ee479cdf8a3b0
Praveen Relli <s160992@rguktsklm.ac.in>
2022-04-05 15:52:39 +0530

    dashboard init d1
    dashboard init d1

---
+++ admin/dashboard_view_pages/dashboard/includes/leftbar.php
@@ -0,0 +1,25 @@
+ <div class="main-menu menu-fixed menu-dark menu-accordion menu-shadow" data-scroll-to-active="true">
+    <div class="main-menu-content">
+      <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
+        <li class=" nav-item"><a href="dashboard.php"><i class="la la-home"></i><span class="menu-title" data-i18n="nav.dash.main">Dashboard</span></a>
+        </li>
+     <li class=" nav-item"><a href="#"><i class="la la-envelope"></i><span class="menu-title" data-i18n="nav.page_headers.main">Queries</span></a>
+          <ul class="menu-content">
+            <li><a class="menu-item" href="todays-contact.php" data-i18n="nav.page_headers.headers_breadcrumbs_basic">Today Queries</a>
+            </li>
+            <li><a class="menu-item" href="last-sevendays-contacts.php" data-i18n="nav.page_headers.headers_breadcrumbs_top">Last 7 Days Queries</a>
+            </li>
+        <li><a class="menu-item" href="all-contacts.php" data-i18n="nav.page_headers.headers_breadcrumbs_top">
+        All Queries</a>
+            </li>
+          </ul>
+        </li>
+
+  
+
+
+
+   
+      </ul>
+    </div>
+  </div>
