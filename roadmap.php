8798e3cebcecce78714885a3dee07fe755b0834b
deepak <john.17.dc@gmail.com>
2022-05-17 21:16:24 +0530

    fixed icon placement on roadmap

--- roadmap.php
+++ roadmap.php
@@ -13,7 +13,7 @@
                 <div class="m-5"></div>
                 
                 <div class="col-8 offset-md-2">
-                    <img src="assets/road1.png" class="img-fluid d-inline" style="height: 60px!important; margin-left: -60px;" alt="">
+                    <img src="assets/road1.png" class="img-fluid d-inline" style="height: 60px!important; margin: -20px -10px 0 -60px;" alt="">
                     <h2 class="text-white d-inline px-3 py-1 border rounded-3" style="background-color:#FBB03B">Assessment</h2>
                     <p class="lead my-2">
                         An assessment test is conducted to understand the basic aptitude level of the student. This also lets us know student's weak and strong areas.  All these  information helps to design a personlised  plan for the student. Also it helps in conducting the mentoring sessions.
@@ -23,7 +23,7 @@
 
                     <div class="text-end">
                         <h2 class="text-white d-inline px-3 py-1 border rounded-3 text-end" style="background-color:#E52D27">Concept Building</h2>
-                        <img src="assets/road2.png" class="img-fluid d-inline" style="height: 60px!important; margin-left: 0px;" alt="">
+                        <img src="assets/road2.png" class="img-fluid d-inline" style="height: 60px!important; margin: -20px 0 0 -10px" alt="">
                     </div>
                     <p class="lead">
                         300+ hours of recorded sessions, personalized modules, 100+ hours of live classes and one-on-one mentoring and doubt clearing session enables the student  in strong concept building.  Student are helps with any other  additional assistant if they feel the need for.
@@ -31,7 +31,7 @@
 
                     <div class="m-5"></div>
                     
-                    <img src="assets/road4.png" class="img-fluid d-inline" style="height: 60px!important; margin-left: -60px;" alt="">
+                    <img src="assets/road4.png" class="img-fluid d-inline" style="height: 60px!important; margin: -20px -10px 0 -60px;" alt="">
                     <h2 class="text-white d-inline px-3 py-1 border rounded-3" style="background-color:#FF5757">Application Building</h2>
                     <p class="lead">
                     Regular assessment tests to check the students aptitude level and concept quizzes before and after of every live class helps the students to develop the application building of the concepts taught in live classes.
@@ -41,7 +41,7 @@
                     
                     <div class="text-end">
                         <h2 class="text-white d-inline px-3 py-1 border rounded-3" style="background-color:#3D5A98">Test & Quizzes</h2>
-                        <img src="assets/road3.png" class="img-fluid d-inline" style="height: 60px!important; margin-left: 0px;" alt="">
+                        <img src="assets/road3.png" class="img-fluid d-inline" style="height: 60px!important; margin: -20px 0 0 -10px" alt="">
                     </div>
                     <p class="lead">
                         30 mandatory full length mocks, 50+ optional mocks, sectional and topic wise mocks prepares the students for the exam day obstacles.
@@ -49,7 +49,7 @@
 
                     <div class="m-5"></div>
                     
-                    <img src="assets/road5.png" class="img-fluid d-inline" style="height: 60px!important; margin-left: -60px;" alt="">
+                    <img src="assets/road5.png" class="img-fluid d-inline" style="height: 60px!important; margin: -20px -10px 0 -60px;" alt="">
                     <h2 class="text-white d-inline px-3 py-1 border rounded-3" style="background-color:#5E17EB">Personal mentoring</h2>
                     <p class="lead">
                     Personal mentoring guides the students at every step of the preparation journey. It enables us to track student progress and identify in which area student is lacking. All this helps us to prepare our students for the exam in a better way.
@@ -59,7 +59,7 @@
 
                     <div class="text-end">
                         <h2 class="text-white d-inline px-3 py-1 border rounded-3" style="background-color:#EA5823">Mock Test & Analysis</h2>
-                        <img src="assets/road6.png" class="img-fluid d-inline" style="height: 60px!important;" alt="">
+                        <img src="assets/road6.png" class="img-fluid d-inline" style="height: 60px!important; margin: -20px 0 0 -10px" alt="">
                     </div>
                     <p class="lead">
                     We have identified various key performance indicators to analyze the mock test results and identify with which areas student is lacking. These key performance indicators include score, accuracy, average speed, etc.
