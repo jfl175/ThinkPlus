ac3c561b5bfeb811fdaca6d4d8d2d974fc38022a
deepak <john.17.dc@gmail.com>
2022-05-20 02:59:47 +0530

    test

--- config.php
+++ config.php
@@ -1,5 +1,12 @@
 <?php 
-    
+    var_dump($_SERVER);
+    exit();
+    if($_SERVER["HTTPS"] != "on")
+    {
+        header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+        exit();
+    }
+
     error_reporting(E_ALL & ~E_NOTICE);
     date_default_timezone_set('Asia/Calcutta');
     
