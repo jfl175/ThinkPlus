2e20179525fb87444db53e8315269ac49b574652
deepak <john.17.dc@gmail.com>
2022-05-19 21:34:20 +0530

    fixed date in popup forms

--- index.php
+++ index.php
@@ -99,7 +99,7 @@
                                 <input type="email" class="form-control" id="bookEmail" name="bookEmail" required placeholder="Email address">
                             </div>
                             <div class="mb-3">
-                                <input type="date" class="form-control" id="bookDate" name="bookDate" min="2022-04-26" value="2022-04-27" required placeholder="Prefferred date">
+                                <input type="date" class="form-control" id="bookDate" name="bookDate" min="<?php echo date('Y-m-j',strtotime("+1 days"));?>" value="<?php echo date('Y-m-j',strtotime("+1 days"));?>" required placeholder="Prefferred date">
                             </div>
                     </div>
                     <div class="modal-footer">
