cacbcd8a789f09d3d2dcdeb1ed5519421b922994
deepak <john.17.dc@gmail.com>
2022-05-20 03:03:16 +0530

    test

--- config.php
+++ config.php
@@ -1,13 +1,14 @@
 <?php 
-    var_dump($_SERVER);
-    exit();
-    if($_SERVER["HTTPS"] != "on")
+
+    error_reporting(E_ALL & ~E_NOTICE);
+
+    if($_SERVER["HTTPS"] != "on" AND $_SERVER['CONTEXT_DOCUMENT_ROOT'] != "C:/wamp64/www/ThinkPlus")
     {
         header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
         exit();
     }
 
-    error_reporting(E_ALL & ~E_NOTICE);
+    
     date_default_timezone_set('Asia/Calcutta');
     
 ?>
